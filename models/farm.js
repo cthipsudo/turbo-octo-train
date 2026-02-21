7e87921ce12ec5cc0e2fd4373cf16a9779317a6d
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-02-21 16:57:01 -0500

    added functionality to delete farms

--- models/farm.js
+++ models/farm.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
+const Product = require("./product");
 const farmSchema = new Schema({
   name: {
     type: String,
@@ -19,6 +19,20 @@ const farmSchema = new Schema({
       ref: "Product",
     },
   ],
+});
+
+// Does not have access to date after deletion
+// farmSchema.pre("findOneAndDelete", async function (data) {
+//   console.log("PRE MIDDLEWARE");
+//   console.log(data);
+// });
+
+// Has access to data after deletion
+farmSchema.post("findOneAndDelete", async function (farm) {
+  if (farm.products.length) {
+    const res = await Product.deleteMany({ _id: { $in: farm.products } });
+    console.log(res);
+  }
 });
 
 const Farm = mongoose.model("Farm", farmSchema);
